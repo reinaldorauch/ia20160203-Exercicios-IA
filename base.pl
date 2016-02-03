04323a0cd8eb7b47fe6ddd3044431065182b1ec4
Reinaldo A. C. Rauch <reinaldorauch@gmail.com>
2016-02-03 21:56:08 -0200

    Implementado exercício 2 e parte do 3

--- base.pl
+++ base.pl
@@ -11,28 +11,51 @@
 %% página 31: exercícios 10.13 a 10.16. (feitos em ... e ...) - Assunto: cortes
 %% em Prolog.
 
+/**
+ * Facts
+ */
 genitor(pam,bob).
 genitor(tom,bob).
 genitor(tom,liz).
 genitor(bob,ann).
 genitor(bob,pat).
 genitor(pat,jim).
+genitor(ron, tonya).
+genitor(hermione, tonya).
+genitor(tonya, ann).
+genitor(tonya, pat).
 
 mulher(pam).
 mulher(liz).
 mulher(pat).
 mulher(ann).
+mulher(hermione).
+mulher(tonya).
 homem(tom).
 homem(bob).
 homem(jim).
+homem(ron).
 
-prole(Y,X) :− genitor(X,Y).
+/**
+ * Rules
+ */
+prole(Y,X) :- genitor(X,Y).
 
-mae(X,Y) :− genitor(X,Y), mulher(X).
+mae(X,Y) :- genitor(X,Y), mulher(X).
 
-avos(X,Z) :− genitor(X,Y), genitor(Y,Z).
+avos(X,Z) :- genitor(X,Y), genitor(Y,Z).
 
-irma(X,Y) :− genitor(Z,X), genitor(Z,Y), mulher(X), not(X = Y).
+irma(X,Y) :- genitor(Z,X), genitor(Z,Y), mulher(X), not(X = Y).
 
-descendente(X,Z) :− genitor(X,Z).
-descendente(X,Z) :− genitor(X,Y), descendente(Y,Z).
+descendente(X,Z) :- genitor(X,Z).
+descendente(X,Z) :- genitor(X,Y), descendente(Y,Z).
+
+irmao(X,Y) :- genitor(Z,X), genitor(Z,Y), homem(X), not(X = Y).
+
+tio(X,Y) :- irmao(X,Z), genitor(Z, Y).
+
+tia(X,Y) :- irma(X,Z), genitor(Z, Y).
+
+avos_paternos(X,Y) :- genitor(X, Z), genitor(Z, Y), homem(Z).
+
+avos_maternos(X,Y) :- genitor(X, Z), genitor(Z, Y), mulher(Z).
